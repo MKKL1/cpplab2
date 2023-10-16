c5fe1c1cbc4ff9d15a2af712986f5c7482f1a31c
Krystian Zielonka <s97779@pollub.edu.pl>
2023-10-16 19:18:23 +0200

    lab2 figure tab

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -9,14 +9,13 @@
 using namespace std;
 
 int main() {
-    Figure* f1 = new Square(4);
-    Figure* f2 = new Circle(2);
-    f1->calculateArea();
-    f1->show();
+    Figure* tab[3];
+    tab[0] = new Square(4);
+    tab[1] = new Square(2);
+    tab[2] = new Circle(5);
 
-    f2->calculateArea();
-    f2->show();
-
-    delete f1;
-    delete f2;
+    for(int i = 0; i < 3; i++) {
+        tab[i]->calculateArea();
+        delete tab[i];
+    }
 }
